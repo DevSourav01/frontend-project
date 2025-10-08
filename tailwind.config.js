88a0a42747eb80c0b4241669b7ceff779aeb3d1b
Sourav Nandi <sn9378115@gmail.com>
2025-10-08 21:27:23 +0530

    do some minor changes padding inhero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
